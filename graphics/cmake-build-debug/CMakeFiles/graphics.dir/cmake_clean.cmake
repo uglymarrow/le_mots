235a0bb91333c34038dfa1d96a3b6feeacff80e5
Ksenia <kalagaevaxenia@ya.ru>
2021-11-15 00:04:59 +0300

    Подключила SFML библиотеку (жесть)

---
+++ graphics/cmake-build-debug/CMakeFiles/graphics.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/graphics.dir/main.cpp.o"
+  "graphics"
+  "graphics.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/graphics.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ graphics/main.cpp
@@ -0,0 +1,22 @@
+#include <SFML/Graphics.hpp>
+
+int main(int argc, char const *argv[]) {
+  sf::RenderWindow window(sf::VideoMode(400, 400), "Hello From SFML");
+  sf::CircleShape shape(100.f);
+  shape.setFillColor(sf::Color::Magenta);
+
+  while (window.isOpen()) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) {
+        window.close();
+      }
+    }
+    window.clear();
+    window.draw(shape);
+    window.display();
+
+  }
+
+  return 0;
+}
