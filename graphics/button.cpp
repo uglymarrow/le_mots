185eaf899fba3f3a6e127391960fc88d5e6c94d5
Ksenia <kalagaevaxenia@ya.ru>
2021-11-18 17:31:01 +0300

    Delete mistake

--- graphics/button.cpp
+++
@@ -1,7 +0,0 @@
-#include "button.h"
-#include <QString>
-
-ColoredButton::ColoredButton(QString string, QWidget *parent):QPushButton(string, parent) {
-    this->setStyleSheet("background-color:rgb(197, 188, 237)");
-}
-
